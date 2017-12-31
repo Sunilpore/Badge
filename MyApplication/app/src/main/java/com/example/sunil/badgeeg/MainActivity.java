7a012fd6a7b210484eefb37f6929dffcdc34378d
Sunilpore <sunilpore20@gmail.com>
2017-12-31 12:18:05 +0530

    Quantity properly update on Toolbar badge. Upload on 31 Dec 17 at 12:17 p.m

--- MyApplication/app/src/main/java/com/example/sunil/badgeeg/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sunil/badgeeg/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.sunil.badgeeg;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -21,24 +17,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button incrStraw,incrLab,incrCal;
     Button decrStraw,decrLab,decrCal;
     Button clear;
-    View actionView;
 
-    Toolbar myToolbar;
-    MenuItem notifyMenuItem;
-
-    ImageButton mImgBt;
-    TextView mCount;
-    int sumCount;
-
-    static int Straw,Lab,Cal;
+    static int Straw,Lab,Cal,test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        myToolbar= (Toolbar) findViewById(R.id.toolbar_lay);
-        setSupportActionBar(myToolbar);
 
         tvStraw= (TextView) findViewById(R.id.badge_notify1);
         tvLab= (TextView) findViewById(R.id.badge_notify2);
@@ -70,24 +55,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu_layout,menu);
-        notifyMenuItem=menu.findItem(R.id.count_menuitem);
-            actionView=notifyMenuItem.getActionView();
-
-            if(actionView!=null){
-                mImgBt=actionView.findViewById(R.id.ib_notification_base);
-                mCount=actionView.findViewById(R.id.tv_notification);
-            }
-
-            mImgBt.setOnClickListener(this);
-
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
     public void onClick(View view) {
 
         switch(view.getId()){
@@ -95,9 +62,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.strawberry_incr:
                 if(tgStraw.isChecked()){
                     Straw++;
-                    sumCount++;
                     tvStraw.setText(String.valueOf(Straw));
-                    mCount.setText(String.valueOf(sumCount));
                 }
                 break;
 
@@ -172,10 +137,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Cal=0;
                     tvCal.setText(String.valueOf(Cal));
                 }
-                break;
-
-            case R.id.ib_notification_base:
-                Toast.makeText(this, "Image Button clicked", Toast.LENGTH_SHORT).show();
                 break;
 
         }
